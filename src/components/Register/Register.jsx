c24a8e576a28b99dc9690ffbf68756a54ceb1a8e
sittikorn-kerdcuen <129787913+sittikorn-kerdchuen@users.noreply.github.com>
2023-10-23 20:48:01 +0700

    Update Register.jsx v1

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,32 +1,32 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import "./Register.css"; //import css
-import { inputStyles } from "./inputStyle";
-//component below
-import Data from "./Data";
-import Addprofile from "./Addprofile";
-import Gender from "./Gender";
-import Height from "./Height";
-import Wieght from "./Wieght";
-import profileDefault from "/src/assets/img/person.png";
-import axios from "axios";
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
+import './Register.css'  //import css
+//component below 
+import Data from './Data';
+import Addprofile from './Addprofile';
+import Gender from './Gender';
+import Height from './Height';
+import Wieght from './Wieght';
+import profileDefault from '/src/assets/img/person.png';
+import axios from 'axios';
+
 
 // IoMaleSharp
 const Register = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [password2, setPassword2] = useState("");
-  const [firstname, setFirstname] = useState("");
-  const [lastname, setLastname] = useState("");
-  const [email, setEmail] = useState("");
-  const [image, setImage] = useState(profileDefault);
-  const [gender, setGender] = useState("");
-  const [height, setHeight] = useState(150);
-  const [weight, setWeight] = useState(40);
-  const [selected, setSelected] = useState(false);
-  const [birthday, setBirthday] = useState("");
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [password2, setPassword2] = useState("")
+  const [firstname, setFirstname] = useState("")
+  const [lastname, setLastname] = useState("")
+  const [email, setEmail] = useState("")
+  const [image, setImage] = useState(profileDefault)
+  const [gender, setGender] = useState("")
+  const [height, setHeight] = useState(150)
+  const [weight, setWeight] = useState(40)
+  const [selected, setSelected] = useState(false)
+  const [birthday, setBirthday] = useState("")
   const [step, setStep] = useState(1);
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   const nextStep = () => {
     setStep(step + 1);
@@ -37,10 +37,10 @@ const Register = () => {
   };
 
   const selectGender = (e) => {
-    setGender(e.target.value);
-    setSelected(!selected);
-  };
-
+    setGender(e.target.value)
+    setSelected(!selected)
+  }
+  
   const handleImageChange = (e) => {
     const selectedImage = e.target.files[0];
     if (selectedImage) {
@@ -55,18 +55,7 @@ const Register = () => {
     }
   };
 
-  const createUser = async (
-    username,
-    password,
-    firstname,
-    lastname,
-    email,
-    gender,
-    height,
-    weight,
-    birthday,
-    image
-  ) => {
+  const createUser = async (username, password, firstname, lastname, email, gender, height, weight, birthday, image) => {
     const requestData = {
       username: username,
       password: password,
@@ -77,41 +66,31 @@ const Register = () => {
       height: height,
       weight: weight,
       birthday: birthday,
-      image: image,
-    };
+      image: image
+    }
 
     const form = new FormData();
     for (const key in requestData) {
       form.append(key, requestData[key]);
     }
-    
+  
+// 
     const response = await axios.post(import.meta.env.VITE_APP_BACKEND_URL+"/register", requestData)
     navigate('/login');
 
-  //   const response = await axios.post(
-  //     "https://healthy-impressed.onrender.com/register",
-  //     requestData
-  //   );
-  //   navigate("/login");
-  // };
+  }
 
   const checkPass = () => {
     if (password === password2) {
-    }
-    return (
-      <p className="p-3 border-red-600 bg-red-300 font-bold">
-        {" "}
-        Password is not match{" "}
-      </p>
-    );
-  };
 
+    }
+    return <p className='p-3 border-red-600 bg-red-300 font-bold'> Password is not match </p>
+  }
   return (
     <main className={`${inputStyles.mainSection}`}>
-
-     
       <form action="/register" className="w-full grid gap-16" method="POST">
-       <section> {step === 1 && (
+        <section>
+          {step === 1 && (
           <Data
             nextStep={nextStep}
             username={username}
@@ -141,8 +120,10 @@ const Register = () => {
 
         {step === 4 && <Height setHeight={setHeight} height={height} />}
         {step === 5 && <Wieght setWeight={setWeight} weight={weight} />}
- </section>
-        <section
+     </section>
+
+        
+      <section
           className={`flex gap-4`}
         >
           {step > 1 && (
