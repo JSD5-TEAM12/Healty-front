bdf07e512fbbf3276749893ca55da573dd9ee847
viic007 <viickub@gmail.com>
2023-10-24 16:35:36 +0700

    add chart

---
+++ src/components/Chart/Listdata.jsx
@@ -0,0 +1,122 @@
+import React from 'react'
+import icon from '../../assets/img/icon.svg'
+
+const Listdata = () => {
+  return (
+    <div className='margin-list w-11/12 h-56 overflow-y-auto'>
+        <div className='w-full flex justify-around mt-4'>
+            <div className='flex items-center'>
+                <img src={icon} alt="" className='h-20'/>
+            </div>
+            <div className='flex items-start justify-center flex-col '>
+                
+                <div>24 May</div>
+                <div>icon 27 minutes</div>
+                <div>2.8 Km</div>
+            </div>
+            <div className='justify-self-start'>
+                <div >
+                    <div><img src="" alt="" />icon</div>
+                    <div>358</div>
+                </div>
+                <div>
+                    <div><img src="" alt="" />icon</div>
+                    <div>42:00</div>
+                </div>
+            </div>
+        </div>
+
+        <div className='w-full flex justify-around mt-4'>
+        <div className='flex items-center'>
+            <img src={icon} alt="" className='h-20'/>
+        </div>
+        <div className='flex items-start justify-center flex-col '>
+            
+            <div>24 May</div>
+            <div>icon 27 minutes</div>
+            <div>2.8 Km</div>
+        </div>
+        <div className='justify-self-start'>
+            <div >
+                <div><img src="" alt="" />icon</div>
+                <div>358</div>
+            </div>
+            <div>
+                <div><img src="" alt="" />icon</div>
+                <div>42:00</div>
+            </div>
+        </div>
+    </div>
+
+    <div className='w-full flex justify-around mt-4'>
+            <div className='flex items-center'>
+                <img src={icon} alt="" className='h-20'/>
+            </div>
+            <div className='flex items-start justify-center flex-col '>
+                
+                <div>24 May</div>
+                <div>icon 27 minutes</div>
+                <div>2.8 Km</div>
+            </div>
+            <div className='justify-self-start'>
+                <div >
+                    <div><img src="" alt="" />icon</div>
+                    <div>358</div>
+                </div>
+                <div>
+                    <div><img src="" alt="" />icon</div>
+                    <div>42:00</div>
+                </div>
+            </div>
+        </div>
+
+        <div className='w-full flex justify-around mt-4'>
+            <div className='flex items-center'>
+                <img src={icon} alt="" className='h-20'/>
+            </div>
+            <div className='flex items-start justify-center flex-col '>
+                
+                <div>24 May</div>
+                <div>icon 27 minutes</div>
+                <div>2.8 Km</div>
+            </div>
+            <div className='justify-self-start'>
+                <div >
+                    <div><img src="" alt="" />icon</div>
+                    <div>358</div>
+                </div>
+                <div>
+                    <div><img src="" alt="" />icon</div>
+                    <div>42:00</div>
+                </div>
+            </div>
+        </div>
+
+        <div className='w-full flex justify-around mt-4'>
+            <div className='flex items-center'>
+                <img src={icon} alt="" className='h-20'/>
+            </div>
+            <div className='flex items-start justify-center flex-col '>
+                
+                <div>24 May</div>
+                <div>icon 27 minutes</div>
+                <div>2.8 Km</div>
+            </div>
+            <div className='justify-self-start'>
+                <div >
+                    <div><img src="" alt="" />icon</div>
+                    <div>358</div>
+                </div>
+                <div>
+                    <div><img src="" alt="" />icon</div>
+                    <div>42:00</div>
+                </div>
+            </div>
+        </div>
+    
+    </div>
+    
+  )
+} 
+
+export default Listdata
