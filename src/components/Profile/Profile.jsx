5524b5eddf0f6f988205dadca4faa95bbc5d1080
viic007 <viickub@gmail.com>
2023-10-24 23:52:25 +0700

    update Profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -14,22 +14,19 @@ const Profile = () => {
 
 
   return (
-    <main className='relative set_index  bottom-10'>
-
-      <section className=' relative set_index_container'>
-        <img className='blur-sm w-full object-contain relative ' src={profile_user} alt="profile_user" />
+    <main className='w-full h-full set-index set-top0 '>
+      <section className=' relative set-index'>
+        <img className='blur-sm object-cover' src={profile_user} alt="profile_user" />
         <div className='overflow-hidden flex justify-center absolute bottom-[-30px] left-[50%] translate-x-[-50%]'>
           <img src={profile_user} className='w-[180px] rounded-full border-4 h-[180px] border-pink-500' alt="profile_user" />
         </div>
       </section>
-
-      <section className=' my-10 '>
+      <section className=' mt-14 '>
         <p className='text-white text-3xl text-center'>{firstname} {lastname} </p>
         <p className='text-white text-3xl text-center'>{`Heighe : ${hight} cm`+` Weight : ${weight} kg`}</p>
       </section>
-
       <section className='mx-5 '>
-        <p className='text-2xl border-b-2 mb-10'>My activity status</p>
+        <p className='text-2xl border-b-2 my-10'>My activity status</p>
         <div className='flex flex-col-reverse gap-5 h-[200px] overflow-y-auto '>
           <div>
             <p className='text-2xl text-center rounded-md  py-2 border-b-2 bg-zinc-800 text-white '>RUNNING</p>
@@ -48,14 +45,10 @@ const Profile = () => {
           </div>
         </div>
       </section>
-
-      <section className='mt-20  flex justify-between mx-5  '>
+      <section className='mt-10 mx-5  flex justify-between   '>
         <button className='block p-5 w-2/5 rounded-md bg-zinc-600 text-2xl font-bold'> <Link to ='/Home'>BACK</Link> </button>
         <button className='block p-5 w-2/5 rounded-md bg-zinc-600 text-2xl font-bold'> <Link to ='/UpdateProfile'> EDIT </Link> </button>
       </section>
-
-
-
     </main>
   )
 }
